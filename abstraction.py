1864e7cb77d7b786ff372c571c1b2ee674cef6f8
Dharun <dharunkumar6381@gmail.com>
2026-04-12 17:31:09 +0530

    Added code in abstraction file

---
+++ abstraction.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+#There is no use of creating object for abstractmethod class:
+class Parent(ABC):
+    @abstractmethod
+    def login(self):
+        print("Lala")
+        
+    @abstractmethod
+    def logout(self):
+        print('Lippa')
+        
+class Child(Parent):
+    def login(self):
+        print("Login success.") #Over Ride
+        
+    def logout(self):
+        print("Logout unsuccess!") #Over Ride
+        
+r = Child()
+r.login()
+r.logout()
