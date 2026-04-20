425ee66f51c0d189f4d606accd54ad19bb196fce
Dharun <dharunkumar6381@gmail.com>
2026-04-20 23:10:57 +0530

    Recursive function

---
+++ recursive_func.py
@@ -0,0 +1,18 @@
+'''
+A recursive function is a function that calls itself  in order to solve a 
+smaller version of the same problem'''
+
+'''
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+
+print(fact(5))
+'''
+
+# Generator and Yield
+'''
+A generator function is a special type of function that uses the yield keyword
+to return values one by one, instead of returning everything at once.
+'''
